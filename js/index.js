ee8e90276cb9020fccd7abaf9d5feb8b449aec40
Anton Zdanov <akumael@gmail.com>
2018-02-08 22:23:31 +0200

    feat: add work section with lightbox

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-$(".port-item").click(function() {
-  $(".collapse").collapse("hide");
+$(function() {
+  $(".port-item").click(function() {
+    $(".collapse").collapse("hide");
+  });
+
+  $(document).on("click", '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
 });
